627c797685fc3a22796465f76a3949376d837602
Dr Anirban Mitra <mitradranirban@gmail.com>
2024-09-05 11:27:20 +0530

    202409051120

--- documentation/image2.py
+++ documentation/image2.py
@@ -16,15 +16,15 @@ import argparse
 
 # Constants, these are the main "settings" for the image
 WIDTH, HEIGHT, MARGIN, FRAMES = 2048, 1024, 128, 1
-FONT_PATH = "fonts/variable/Samaano[wght].ttf"
+FONT_PATH = "fonts/variable/Samaano[wdth,wght].ttf"
 FONT_LICENSE = "OFL v1.1"
 AUXILIARY_FONT = "Helvetica"
 AUXILIARY_FONT_SIZE = 48
 
 LINE_ONE = "ABCDEFGHIJKLMNOPQ"
-LINE_TWO = "RSTUVWXYZ123456789"
-LINE_THREE = "abcdefghijklmnopqrstu"
-LINE_FOUR = "vwxyz,.;:!@#$%^&*(){}[]"
+LINE_TWO = "RSTUVWXYZabcdefghijklm "
+LINE_THREE = "nopqrstuvwxyz,.;:!@#$%^&"
+LINE_FOUR = "अअइईउऊएऐओऔकखगघङचछजझञ"
 BIG_TEXT_FONT_SIZE = 160
 BIG_TEXT_SIDE_MARGIN = MARGIN * 1
 BIG_TEXT_BOTTOM_MARGIN = MARGIN * 5.45
