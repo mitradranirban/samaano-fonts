d6623488246d46403bf85007f802d9031b51f660
Dr Anirban Mitra <mitradranirban@gmail.com>
2024-10-17 14:29:57 +0530

    202410171430

--- sources/ufocreate.sh
+++ sources/ufocreate.sh
@@ -2,11 +2,16 @@
 sfd2ufo Samaano-Bold.sfd Samaano-Bold.ufo
 sfd2ufo Samaano-Wide-Bold.sfd Samaano-Wide-Bold.ufo
 sfd2ufo Samaano-Wide-Thin.sfd Samaano-Wide-Thin.ufo
-sfd2ufo Samaano-Thin.sfd Samaano-Thin.ufo 
+sfd2ufo Samaano-Thin.sfd Samaano-Thin.ufo
+sfd2ufo Samaano-Bold-Slanted.sfd Samaano-Bold-Slanted.ufo
+sfd2ufo Samaano-Wide-Bold-Slanted.sfd Samaano-Wide-Bold-Slanted.ufo
+sfd2ufo Samaano-Wide-Thin-Slanted.sfd Samaano-Wide-Thin-Slanted.ufo
+sfd2ufo Samaano-Thin-Slanted.sfd Samaano-Thin-Slanted.ufo 
 cp Samaano-Bold.ufo/features.fea Samaano-Thin.ufo/
 cp Samaano-Bold.ufo/features.fea Samaano-Wide-Thin.ufo/
 cp Samaano-Bold.ufo/features.fea Samaano-Wide-Bold.ufo/
-ufonormalizer -a Samaano-Bold.ufo
-ufonormalizer -a Samaano-Wide-Bold.ufo
-ufonormalizer -a Samaano-Thin.ufo
-ufonormalizer -a Samaano-Wide-Thin.ufo
+cp Samaano-Bold.ufo/features.fea Samaano-Wide-Bold-Slanted.ufo/
+cp Samaano-Bold.ufo/features.fea Samaano-Wide-Thin-Slanted.ufo/
+cp Samaano-Bold.ufo/features.fea Samaano-Thin-Slanted.ufo/
+cp Samaano-Bold.ufo/features.fea Samaano-Bold-Slanted.ufo/
+
