b5e36ec51b234543c60bc66fea09c06f334db339
Dr Anirban Mitra <mitradranirban@gmail.com>
2024-11-07 15:31:13 +0530

    202411071530

--- sources/addfixed.sh
+++ sources/addfixed.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-DATA="  <key>postscriptIsFixedPitch<\/key>\n    <true\/>\n  <\/dict>\n<\/plist>"
-KEY="<\/dict>\n<\/plist>"
-cd Samaano-Bold.ufo
+DATA="<key>postscriptIsFixedPitch<\/key>\n    <true\/>\n    <key>openTypeOS2Selection<\/key>\n    <array>\n    <integer>7<\/integer>\n   <\/array>\n  <\/dict>\n<\/plist>"
+KEY="  <\/dict>\n<\/plist>"
 sed -i 's/$KEY/$DATA/' fontinfo.plist
 
--- sources/ufocreate.sh
+++ sources/ufocreate.sh
@@ -8,5 +8,3 @@ sfd2ufo Samaano-Wide-Bold-Slanted.sfd Samaano-Wide-Bold-Slanted.ufo
 sfd2ufo Samaano-Wide-Thin-Slanted.sfd Samaano-Wide-Thin-Slanted.ufo
 sfd2ufo Samaano-Thin-Slanted.sfd Samaano-Thin-Slanted.ufo 
 
-           
-
