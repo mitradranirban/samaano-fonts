af56a37360b85a4975b667b36e3313b302e93631
Dr Anirban Mitra <mitradranirban@gmail.com>
2025-06-25 23:01:46 +0530

    completed Adobe Latin 4

--- sources/copyfea.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-cp  features.fea Samaano-Thin.ufo/
-cp  features.fea Samaano-Wide-Thin.ufo/
-cp  features.fea Samaano-Wide-Bold.ufo/
-cp  features.fea Samaano-Wide-Bold-Slanted.ufo/
-cp  features.fea Samaano-Wide-Thin-Slanted.ufo/
-cp  features.fea Samaano-Thin-Slanted.ufo/
-cp  features.fea Samaano-Bold-Slanted.ufo/
-cp  features.fea Samaano-Bold.ufo/
