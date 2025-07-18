290565554bc7cea6dbb47fe840205d80238d646b
Dr Anirban Mitra <mitradranirban@gmail.com>
2025-07-18 22:32:02 +0530

    adding anchors to latin added characters

--- sources/addfixed.sh
+++ sources/addfixed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DATA="<key>postscriptIsFixedPitch<\/key>\n    <true\/>\n    <key>openTypeOS2Selection<\/key>\n    <array>\n    <integer>7<\/integer>\n   <\/array>\n  <\/dict>\n<\/plist>"
 KEY="  <\/dict>\n<\/plist>"
-sed -i 's/$KEY/$DATA/' fontinfo.plist
+sed -i 's/$KEY/$DATA/' Samaano-Bold.ufo/fontinfo.plist
 
